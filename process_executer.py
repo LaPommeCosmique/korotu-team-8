137e963854a76eae1181d7243949fa52a26d2f07
vaibhave2020 <163835305+vaibhave2020@users.noreply.github.com>
2024-03-19 15:22:29 -0700

    Update process_executer.py

--- process_executer.py
+++ process_executer.py
@@ -3,22 +3,15 @@ import time
 
 def run_in_terminal(command):
     p.Popen(["gnome-terminal", "--", "bash", "-c", command])
+    time.sleep(5)
 
 
 roscore = run_in_terminal("roscore")
 
-time.sleep(5)
-
 rplidar = run_in_terminal("roslaunch rplidar_ros rplidar.launch")
-
-time.sleep(5)
 
 mavros = run_in_terminal("roslaunch mavros apm.launch")
 
-time.sleep(5)
-
 scancorrect = run_in_terminal("rosrun scan_correction scan_correction_node")
-
-time.sleep(5)
 
 hectorslam = run_in_terminal("roslaunch hector_slam_launch tutorial.launch")
