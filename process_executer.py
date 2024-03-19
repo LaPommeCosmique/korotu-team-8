d69bda3b36ae335b9d58dbb6fbe5b50ac8f619b5
vaibhave2020 <163835305+vaibhave2020@users.noreply.github.com>
2024-03-19 15:14:43 -0700

    Update process_executer.py

--- process_executer.py
+++ process_executer.py
@@ -9,16 +9,16 @@ roscore = run_in_terminal("roscore")
 
 time.sleep(5)
 
-talker = run_in_terminal("roslaunch rplidar_ros rplidar.launch")
+rplidar = run_in_terminal("roslaunch rplidar_ros rplidar.launch")
 
-time.sleep(1)
+time.sleep(5)
 
 mavros = run_in_terminal("roslaunch mavros apm.launch")
 
-time.sleep(1)
+time.sleep(5)
 
 scancorrect = run_in_terminal("rosrun scan_correction scan_correction_node")
 
-time.sleep(1)
+time.sleep(5)
 
 hectorslam = run_in_terminal("roslaunch hector_slam_launch tutorial.launch")
